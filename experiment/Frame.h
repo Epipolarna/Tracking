579d9b0958d9431c3da868885fdd56175f611605
Gustav Hager <hager.gustav@gmail.com>
2013-02-26 20:46:41 +0000

    Klasser för att hantera det mesta

---
+++ experiment/Frame.h
@@ -0,0 +1,19 @@
+#ifndef __FRAME__
+#define __FRAME__
+
+#include <opencv2/core/core.hpp>
+#include <vector>
+
+using namespace cv;
+
+class Frame{
+public:
+	Mat image;
+	Mat *probabilityMap;
+
+	Frame(Mat img);
+	
+	
+};
+
+#endif

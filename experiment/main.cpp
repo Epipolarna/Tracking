023bcdaf22185895cfa3481a4e98aa0132352939
Gustav Hager <hager.gustav@gmail.com>
2013-02-26 20:46:55 +0000

    Nytt testprogram

--- experiment/main.cpp
+++ experiment/main.cpp
@@ -2,26 +2,31 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+#include "FrameList.h"
+
 using namespace cv;
 using namespace std;
 
-
 int main(int argc, char** argv){
 
-	if(argc != 2){
-		cout << "Usage: display_image ImageToLoadAndDisplay" << endl;
-		return -1;
-	}
-	
-	Mat image;
-	image = imread(argv[1],CV_LOAD_IMAGE_COLOR);
-	if(!image.data){
-		cout << "could not open or find image" << std::endl;
-		return -1;
+	CvCapture *movie;
+	int numFrames;
+	IplImage* frameImage = NULL;
+	int frameNumber = 0;
+
+	FrameList frameList;
+
+	movie = cvCaptureFromFile("../../data/camera1.mov");
+	numFrames = cvGetCaptureProperty(movie,CV_CAP_PROP_FRAME_COUNT);
+
+
+	//read image frameList into frame objects
+	for(int i=0; i < numFrames; i++){
+		frameImage = cvQueryFrame(movie);
+		frameList.appendFrame(frameImage);
 	}
 
-	namedWindow("Display window",CV_WINDOW_AUTOSIZE);
-	imshow("Display window",image);
-	waitKey(0);
+	cout << "read:" << frameList.getNumFrames() << "frames" << endl;
+
 	return 0;
 }

57436649dbfc157e46a2fc24b8e330d0f40f8d31
Martin Svensson <marsv106@student.liu.se>
2013-03-16 23:11:03 +0000

    La till typ.. allt... Looks rather nice imo.

--- Klasser/Frame.h
+++ Klasser/Frame.h
@@ -1,23 +1,54 @@
-#ifndef __FRAME__
-#define __FRAME__
+/* 
+*	frame.h
+*
+*
+*/
 
-#include <opencv2/core/core.hpp>
-#include <vector>
+
+#ifndef FRAME_H
+#define FRAME_H
+
 #include "object.h"
-
-#include "ProbabilityMap.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
 
 using namespace cv;
 
-class Frame{
-public:
-	Frame(Mat img);
+class frame
+{
 
 public:
-	Mat image;
-	ProbabilityMap *p;
+	frame(Mat rawFrame, Mat probMap);
 	
+	//Draws the bounding boxes of the objects in object list.
+	void drawBoundingBox(cv::Scalar color);
+	void getObjects();
+
+	//Image Processing of probabilitymap.
+	void threshMap(int threshval);
+	void openingBinMap(int iterations = 1);
+	void closingBinMap(int iterations = 1);
+	
+	//Data
+	Mat rawFrame;		//Should be 3-channel color
+	Mat probMap;		//Should be single channel grayscale
 	std::list<Object> objects;
+	
+	//For debugging/showoff
+	void showObjects();
+	void showImageRaw(std::string windowID);
+	void showImageProbMap(std::string windowID);
+	
+protected:
+
+private:
+	//Might need these later
+	void erodeBinMap(int iterations = 1);	
+	void dilateBinMap(int iterations = 1);
+
 };
+
 
 #endif

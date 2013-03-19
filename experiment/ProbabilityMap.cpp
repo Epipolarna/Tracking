761842b2627f04e95fbe7cfb4672b1c5b90d8c72
Gustav Hager <hager.gustav@gmail.com>
2013-03-19 16:59:38 +0000

    Probability map, nästan klar

--- experiment/ProbabilityMap.cpp
+++ experiment/ProbabilityMap.cpp
@@ -5,6 +5,7 @@ ProbabilityMap::ProbabilityMap(Frame *prevFrame, Frame *currFrame){
 	
 	numGauss = 3;
 	lambda = 2.5; //this should probably be tweaked to be lower
+	initSigma = 3;
 	int numPixels = currFrame->image.rows * currFrame->image.cols;
 	Mat image = currFrame->image;
 	
@@ -29,11 +30,104 @@ ProbabilityMap::ProbabilityMap(Frame *prevFrame, Frame *currFrame){
 			}
 		}
 	}else{
-		updateDistributions(prevFrame,currFrame);
+		this->distributions = prevFrame->pData->distributions;
+		updateDistributions(image);
 	}
 
+	setB(image.rows,image.cols);
 }
 
-void ProbabilityMap::updateDistributions(Frame *prevFrame, Frame *currFrame){
+void ProbabilityMap::updateDistributions(Mat image){
+	int bestMatch;
+	float d[numGauss];
 
+	//update all distributions for all pixels
+	for(int row=0; row < image.rows; row++){
+		for(int col=0; col < image.cols; col++){
+			//measure distance to each gaussian for this pixel
+			resetDistance(d);
+			bestMatch = -1;
+			gauss3D bestDist;
+			for(int k=0; k < numGauss; k++){
+				gauss3D distK = distributions[row*image.rows+col+k];
+				//for each channel
+				for(int c=0; c < 3; c++){
+					int a =	image.at<Vec3b>(row,col)[c];
+					int b = distK.mean[c];
+					float sigma = distK.sigma[c];
+					d[k] = d[k] + pow((a-b)/sigma,2);
+				}
+
+				if(d[k] < lambda){
+					if(bestMatch == -1){
+						bestDist = distK;
+					}else if(betterMatch(bestDist,distK)){
+						bestDist = distK;
+						bestMatch = k;
+					}
+				}
+			}
+
+			if(bestMatch == -1){//create a new distribution if we did not match at all
+				bestMatch = numGauss;//this should be the worst match instead
+				initDistribution(distributions[row*image.rows+col+bestMatch],
+												 initSigma,image.at<Vec3b>(row,col));
+			}else{
+				float roh;
+				float w = distributions[row*image.rows+col+bestMatch].w;
+				distributions[row*image.rows+col+bestMatch].w = w = (1 - alpha)*w + alpha;
+				roh = alpha/w;
+				for(int c=0; c < 3; c++){
+					distributions[row*image.rows+col+bestMatch].mean[c] = (1 - roh)*distributions[row*image.rows+col+bestMatch].mean[c] + roh*image.at<Vec3b>(row,col)[c];
+					distributions[row*image.rows+col+bestMatch].sigma[c] = (1 - roh)*distributions[row*image.rows+col+bestMatch].sigma[c] + roh*pow(image.at<Vec3b>(row,col)[c] - distributions[row*image.rows+col+bestMatch].mean[c],2);
+				}
+			}
+			float wSum = sumW(row,col,image.rows);
+			for(int k=0; k < numGauss; k++){
+				float w = distributions[row*image.rows+col+k].w;
+				distributions[row*image.rows+col+k].w = w/wSum;
+			}
+		}
+
+	}
+}
+
+Mat ProbabilityMap::setB(int rows, int cols){
+	Mat p = Mat(rows,cols,CV32F);
+}
+
+float ProbabilityMap::sumW(int row, int col,int maxRow){
+	float acc = 0;
+	for(int k=0; k < numGauss; k++){
+		acc = acc + distributions[row*maxRow+col+k].w;
+	}
+	return acc;
+}
+
+void ProbabilityMap::initDistribution(gauss3D &g, uchar sigma, Vec3b mean){
+	for(int i=0; i < 3; i++){
+		g.sigma[i] = sigma;
+		g.mean[i] = mean[i];
+	}
+	g.w = 1/numGauss;
+}
+
+float ProbabilityMap::sigmaSize(gauss3D g){
+	float acc = 0;
+	for(int i=0; i < 3; i++){
+		acc = acc + sqrt(g.sigma[i]);
+	}
+	return acc;
+}
+
+bool ProbabilityMap::betterMatch(gauss3D bestMatch, gauss3D thisone){
+	return thisone.w/sigmaSize(thisone) > bestMatch.w/sigmaSize(bestMatch);
+}
+
+void ProbabilityMap::resetDistance(float *d){
+	for(int i=0; i < numGauss; i++){
+		for(int c=0; c < 3; c++){
+			d[i] = 0;
+		}
+	}
 }
--- experiment/ProbabilityMap.h
+++ experiment/ProbabilityMap.h
@@ -23,15 +23,20 @@ public:
 	Mat pImage;
 
 	ProbabilityMap(Frame *prevFrame, Frame *currFrame);
-	
+	Mat setB(int rows, int cols);
 private:
 	int numGauss; //how many distributions will be used
 	float lambda; //treshold for belonging to a distribution
 	float initSigma; //inital sigma for new distribution
+	float alpha;
 	
 	gauss3D *distributions;
-	
-	void updateDistributions(Frame *prevFrame,Frame *currFrame);
+	void updateDistributions(Mat image);
+	void resetDistance(float *d);
+	bool betterMatch(gauss3D bestDist, gauss3D distK);
+	void initDistribution(gauss3D &g, uchar sigma, Vec3b mean);
+	float sigmaSize(gauss3D g);
+	float sumW(int row, int col, int maxRow);
 };
 
 #endif

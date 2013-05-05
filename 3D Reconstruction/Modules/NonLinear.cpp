a72749e2ecee6b3a354cfef8e520470d3f9e1e9a
Martin Svensson <marsv106@student.liu.se>
2013-05-05 15:23:41 +0000

    Bundle adjustment (ej testad ännu), but since it compiles, I'll ship it!

--- 3D Reconstruction/Modules/NonLinear.cpp
+++ 3D Reconstruction/Modules/NonLinear.cpp
@@ -98,7 +98,7 @@ namespace NonLinear
 	cv::Mat NonLinear::goldNonLin(cv::Mat _F, cv::Mat C1, cv::Mat C2, cv::Mat _points3D, std::vector<cv::Point2d> _points1, std::vector<cv::Point2d> _points2)
 	{
 		int n3Dpoints = max(_points3D.size().width, _points3D.size().height);
-		int nPoints = _points1.size();
+		int nPoints = (int)_points1.size();
 		goldNonLinData adata;
 		std::vector<double> params;
 		
@@ -139,23 +139,15 @@ namespace NonLinear
 
 
 
-
-
-
-
-
-
-
-
 	void NonLinear::goldStandardRefine(cv::Mat _F, vector<Point2d> _points1, vector<Point2d> _points2)
 	{
-		int nPoints = _points1.size();
+		int nPoints = (int)_points1.size();
 		cv::Mat F = _F;
 		goldStandardData adata;
 		
 		
-		Mat points1(3, _points1.size(), CV_64FC1);
-		Mat points2(_points1.size(), 3, CV_64FC1);
+		Mat points1(3, (int)_points1.size(), CV_64FC1);
+		Mat points2((int)_points1.size(), 3, CV_64FC1);
 		for(int i = 0; i < _points1.size(); ++i)
 		{
 			points1.at<double>(0,i) = _points1[i].x;
@@ -183,4 +175,125 @@ namespace NonLinear
 	}
 
 
+	void BAResiduals(double* p, double* error, int m, int n, void* derp)
+	{
+		int errorIdx = 0;
+		int paramIdx = 0;
+		BAData* data = static_cast<BAData*>(derp);
+		//Rebuild 3Dpoints (sigh)
+		//data->all3DPoints = Mat(3,data->n3DPoints,CV_64FC1,&p[data->nViews*6]);
+		for(int i = 0; i < data->n3DPoints; i++)
+		{
+			(*data->all3DPoints)[i].x = p[data->nViews*6 + 3*i];
+			(*data->all3DPoints)[i].x = p[data->nViews*6 + 3*i + 1];
+			(*data->all3DPoints)[i].x = p[data->nViews*6 + 3*i + 2];
+		}
+
+		// For all cameras
+		for (int i = 0; i < data->rotations.size(); i++)
+		{
+			//Extract rotation and translation vectors
+			setDataRange(p,data->rVec.ptr<double>(),i*6,i*6+3);
+			setDataRange(p,data->t.ptr<double>(),i*6+3,(i+1)*6);
+			
+			Rodrigues(data->rVec, data->R);
+			hconcat(data->R, data->t, data->C);
+			data->P = data->K * data->C;
+			
+			// for all visible 3D points, calculate the reprojection error
+			for(int j = 0; j < data->points3D[i].size(); j++)
+			{
+				// Get the 3D point in hom. coords
+				data->point3D.ptr<double>()[0] = data->points3D[i][j]->x;
+				data->point3D.ptr<double>()[1] = data->points3D[i][j]->y;
+				data->point3D.ptr<double>()[2] = data->points3D[i][j]->z;
+				data->point3D.ptr<double>()[3] = 1;
+				
+				// Project onto image plane
+				data->point2D = data->P * data->point3D;
+				//Calculate residuals.
+				error[errorIdx] = (*data->imagePoints[i])[j].x - data->point2D.ptr<double>()[0] / data->point2D.ptr<double>()[2];
+				error[errorIdx+1] = (*data->imagePoints[i])[j].y - data->point2D.ptr<double>()[1] / data->point2D.ptr<double>()[2];
+				errorIdx += 2;
+			}
+		}
+	}
+
+
+	void NonLinear::BundleAdjust(std::vector<Camera*> views, std::vector<cv::Point3d>* _all3DPoints)
+	{
+		int nPoints = 0;
+		//I want this :/
+		//cv::Mat ALL3DPOINTS(3,1337,CV_64FC1);
+		//data.n3DPoints = ALL3DPOINTS.size().width;
+		
+		BAData data;
+		cv::Mat rVec;
+		std::vector<double> params;
+		int residualTerms = 0;
+		int pointcounter = 0;
+		data.K = K;
+		data.nViews = (int)views.size();
+		
+
+		//preallocate everything
+		double oneData[1] = {1};
+		data.one = cv::Mat(1,1,CV_64FC1,oneData);
+		data.R = cv::Mat(3,3,CV_64FC1);
+		data.rVec = cv::Mat(1,3,CV_64FC1);
+		data.t = cv::Mat(1,3,CV_64FC1);
+		data.point2D = cv::Mat(1,3,CV_64FC1);
+		data.point3D = cv::Mat(1,4,CV_64FC1);
+		data.C = cv::Mat(3,4,CV_64FC1);
+		data.P = cv::Mat(3,4,CV_64FC1);
+		data.all3DPoints = _all3DPoints;
+		
+		//Save data.
+		for(std::vector<Camera*>::iterator it = views.begin(); it != views.end(); it++)
+		{
+			Rodrigues((*it)->R, rVec);
+			data.rotations.push_back(rVec);
+			data.translations.push_back((*it)->t);
+			data.imagePoints.push_back(&((*it)->imagePoints));
+			data.points3D.push_back(((*it)->visible3DPoints));
+			residualTerms += (int)(*it)->imagePoints.size()*2;
+		}
+		
+		//For all cameras
+		std::vector<cv::Mat>::iterator itTrans = data.translations.begin();
+		for (std::vector<cv::Mat>::iterator itRot = data.rotations.begin(); itRot != data.rotations.end(); itRot++)
+		{
+			// Rotation parameters
+			params.push_back(itRot->ptr<double>()[0]);
+			params.push_back(itRot->ptr<double>()[1]);
+			params.push_back(itRot->ptr<double>()[2]);
+			// Translation paramters
+			params.push_back(itTrans->ptr<double>()[0]);
+			params.push_back(itTrans->ptr<double>()[1]);
+			params.push_back(itTrans->ptr<double>()[2]);
+			// 3D points
+		}
+		//All 3D points
+		/*
+		for(MatIterator_<double> matIt = ALL3DPOINTS.begin<double>(); matIt != ALL3DPOINTS.end<double>(); ++matIt)
+		{
+			params.push_back(*matIt);
+		}*/
+		for(std::vector<cv::Point3d>::iterator it = data.all3DPoints->begin(); it != data.all3DPoints->end(); it++)
+		{
+			params.push_back(it->x);
+			params.push_back(it->y);
+			params.push_back(it->z);
+		}
+
+
+		double* paramArray = params.data();
+		double info[LM_INFO_SZ];
+		int ret;
+		ret = dlevmar_dif(BAResiduals, paramArray, NULL, (int)params.size(),residualTerms,10000,NULL,info,NULL,NULL,&data);
+		printf("Levenberg-Marquardt returned in %g iter, reason %g, output error %g with an initial error of [%g]\n", info[5], info[6], info[1], info[0]);
+	}
+
+
+	
 }
--- 3D Reconstruction/Modules/NonLinear.h
+++ 3D Reconstruction/Modules/NonLinear.h
@@ -7,6 +7,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <libs/levmar/levmar.h> //<<<---- numera på sven.
+#include "Camera.h"
+#include "Estimate3D.h"
 
 
 namespace NonLinear
@@ -26,6 +28,8 @@ namespace NonLinear
 
 		std::vector<double>* params;
 
+		
+		
 		cv::Mat F;
 		cv::Mat K;
 		std::vector<cv::Point2d> points1; 
@@ -33,6 +37,33 @@ namespace NonLinear
 		cv::Mat points3D;
 		cv::Mat C1;
 		cv::Mat C2;
+	};
+
+	struct BAData
+	{
+		int nViews;
+		int n3DPoints;
+		
+		double oneData;
+		cv::Mat one;
+		cv::Mat R;
+		cv::Mat rVec;
+		cv::Mat t;
+		cv::Mat point2D;
+		cv::Mat point3D;
+		cv::Mat C;
+		cv::Mat P;		
+
+		cv::Mat K;
+		std::vector<cv::Mat> rotations;
+		std::vector<cv::Mat> translations;
+		std::vector<std::vector<cv::Point2d>*> imagePoints;
+		//cv::Mat allPoints3D;
+		//std::vector<std::vector<cv::Mat>> visible3Dpoints;
+		std::vector<std::vector<cv::Point3d*>> points3D;
+		std::vector<cv::Point3d>* all3DPoints;
+		
+
 	};
 
 
@@ -44,6 +75,7 @@ namespace NonLinear
 		void testFunc(double* params, double* residuals);
 		void goldStandardRefine(cv::Mat F, std::vector<cv::Point2d> points1, std::vector<cv::Point2d> points2);
 		cv::Mat goldNonLin(cv::Mat F, cv::Mat C1, cv::Mat C2, cv::Mat point3D, std::vector<cv::Point2d> points1, std::vector<cv::Point2d> points2);
+		void BundleAdjust(std::vector<Camera*> views, std::vector<cv::Point3d>* all3DPoints);
 	private:
 		// Hard-coded in constructor
 		cv::Mat K;

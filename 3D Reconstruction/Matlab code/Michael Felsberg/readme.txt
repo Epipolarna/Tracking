a46ac59f7704c4135e969fbe78103de9b3cd9d48
AlexanderSjoholm <Alexander.Sjoholm@gmail.com>
2013-10-15 12:58:54 +0200

    Update

--- 3D Reconstruction/Matlab code/Michael Felsberg/readme.txt
+++
@@ -1,5 +0,0 @@
-All code is © CVL.
-
-Optical flow code can be found here:
-http://cs.brown.edu/~black/code.html
-
--- 3D Reconstruction/Modules/Visualization/myLoadObj.h
+++
@@ -1,5 +0,0 @@
-// Player.h, Container for player related actions and information
-
-#include "Model.h"
-	
-void myLoadObj(char* filename, vis::Model* model);
